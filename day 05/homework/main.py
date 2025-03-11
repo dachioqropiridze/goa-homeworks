b38998b04f1c2713a4dc5362ae12e851a7e7c7e7
dachioqropiridze <oqropiridzedachi@gmail.com>
2025-03-11 08:55:09 -0700

    commit

--- day 05/homework/main.py
+++ day 05/homework/main.py
@@ -1,44 +1,44 @@
 #დავალება 2:
 
-# password = "dachi123"
-# answer = input ("enter your password: ")
+password = "dachi123"
+answer = input ("enter your password: ")
 
-# if answer == password:
-#     print("acces granted")
-# else: print("pasword is incorect")    
-
-
-
-
-# #დავალება 3:
-
-# num1 = int (input("enter any number: "))
-# if num1 %2 == 0: 
-#     print("number is even")
-# else: print("number is odd")
-
-#დავალება 4: 
+if answer == password:
+    print("acces granted")
+else: print("pasword is incorect")    
 
 
 
-# num2 = int (input("enter number: "))
-# num3 = int (input("enter number: "))
-# num4 = int (input("enter number: "))
-# num5 = int (input("enter number: "))
-# num6 = int (input("enter number: "))
 
-# result = num2 + num3 + num4 + num5 + num6 / 5
-# print(result)
+# დავალება 3:
 
+num1 = int (input("enter any number: "))
+if num1 %2 == 0: 
+    print("number is even")
+else: print("number is odd")
 
-#დავალება 5: 
-
-# temperature1 =int (input("enter any temperature"))
-# result1 = temperature1 * 2 + 30
-# print(result1)
+# დავალება 4: 
 
 
-#დავალება 6: 
+
+num2 = int (input("enter number: "))
+num3 = int (input("enter number: "))
+num4 = int (input("enter number: "))
+num5 = int (input("enter number: "))
+num6 = int (input("enter number: "))
+
+result = num2 + num3 + num4 + num5 + num6 / 5
+print(result)
+
+
+# დავალება 5: 
+
+temperature1 =int (input("enter any temperature"))
+result1 = temperature1 * 2 + 30
+print(result1)
+
+
+# დავალება 6: 
 
 temperature2 = int (input("enter any temperature"))
 result2 = (temperature2 - 30) / 2
